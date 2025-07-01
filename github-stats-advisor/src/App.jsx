e21dadc37297d03f4a967f8c0c84e1a6ab5be437
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-07-01 21:57:46 +0300

    h2 added

--- github-stats-advisor/src/App.jsx
+++ github-stats-advisor/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-    <div className="h-[400px]  mt-4"> {/* or h-screen, h-96, etc */}
+  <div className="h-[400px] mt-4 relative">
   <Squares 
     speed={0.2} 
     squareSize={40}
@@ -17,8 +17,20 @@ function App() {
     borderColor="#fff"
     hoverFillColor="#222"
   />
+
+  {/* Overlay content */}
+  <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
+    <h2 className="text-4xl md:text-3xl font-semibold mb-4">
+      Get Personalized GitHub Growth Advice
+    </h2>
+    <input 
+      type="text" 
+      placeholder="Enter your GitHub username" 
+      className="px-4 py-2 rounded-md text-black bg-white  w-full max-w-sm"
+    />
+  </div>
 </div>
- 
+
     </> 
   )
 }
