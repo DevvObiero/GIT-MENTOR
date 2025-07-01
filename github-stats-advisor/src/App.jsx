684e9651fc75cf4d6069b99755c9af19e25ec642
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-07-01 22:14:42 +0300

    sses

--- github-stats-advisor/src/App.jsx
+++ github-stats-advisor/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-  <div className="h-[400px] mt-4 relative">
+<div className="h-[400px] mt-4 relative">
   <Squares 
     speed={0.2} 
     squareSize={40}
@@ -19,14 +19,14 @@ function App() {
   />
 
   {/* Overlay content */}
-  <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
+  <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4 pointer-events-none">
     <h2 className="text-4xl md:text-3xl font-semibold mb-4">
       Get Personalized GitHub Growth Advice
     </h2>
     <input 
       type="text" 
       placeholder="Enter your GitHub username" 
-      className="px-4 py-2 rounded-md text-black bg-white  w-full max-w-sm"
+      className="px-4 py-2 rounded-md text-black bg-white w-full max-w-sm pointer-events-auto"
     />
   </div>
 </div>
