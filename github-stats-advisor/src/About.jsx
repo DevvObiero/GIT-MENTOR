c4b06f917263102fcc2301f00ae6dd272f812bdc
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-07-02 15:11:49 +0300

    done with the about page

--- github-stats-advisor/src/About.jsx
+++ github-stats-advisor/src/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
 
 
-    <div className="max-w-3xl mx-auto p-6 mt-3.5">
+    <div className="max-w-4xl mx-auto p-6 mt-3.5">
      
       <ShinyText
   text="About GitHub Mentor"
