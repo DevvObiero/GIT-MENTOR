50020850cf919ff9b217cf31c8f115157298049b
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-07-27 23:42:18 +0300

    changed the port

--- github-stats-advisor/server/index.js
+++ github-stats-advisor/server/index.js
@@ -190,4 +190,4 @@ Give 3 actionable tips to improve my GitHub profile. Make sure to mention all th
   }
 });
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
